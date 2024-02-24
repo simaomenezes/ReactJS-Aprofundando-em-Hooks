b14c1d8046403b0d444e78188a2e947c527dcc96
Simão Neto <simaomenezesneto@gmail.com>
2024-02-24 11:46:26 +0000

    feat: Create GlobalStyle

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -2,6 +2,7 @@ import { Button } from "./components/Button";
 
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
@@ -11,6 +12,8 @@ export function App() {
       <Button variant="secondary"/>
       <Button variant="success"/>
       <Button/>
+
+      <GlobalStyle/>
     </ThemeProvider>
   )
 }
