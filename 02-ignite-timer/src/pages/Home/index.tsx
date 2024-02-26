86d71a30a6fc551fb40c9c38eff885ceb1531337
Simão Neto <simaomenezesneto@gmail.com>
2024-02-26 21:44:39 +0000

    feat: Change title page, make other things

--- 02-ignite-timer/src/pages/Home/index.tsx
+++ 02-ignite-timer/src/pages/Home/index.tsx
@@ -45,13 +45,19 @@ export function Home() {
   const id = String(new Date().getTime())
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
+  let interval: number
+
   useEffect(() => {
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondPassed(
           differenceInSeconds(new Date(), activeCycle.startDate),
         )
-      }, 100)
+      }, 1000)
+    }
+
+    return () => {
+      clearInterval(interval)
     }
   }, [activeCycle])
 
@@ -64,6 +70,7 @@ export function Home() {
     }
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
+    setAmountSecondPassed(0)
 
     reset()
   }
@@ -76,6 +83,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [minutes, seconds, activeCycle])
 
   const task = watch('task')
   const isSubmitDisabled = !task
